6cd44014f4db125668a48e83f13eb640e4e7ae99
memorial-roger <Roger_Sean2025>
2025-11-11 08:55:40 +0530

    Vite File Changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Roger-Sean-Cudney-Bueno-memorial/",
+  base: "/Roger-Sean-Cudney-Bueno-Memorial/", 
 });
